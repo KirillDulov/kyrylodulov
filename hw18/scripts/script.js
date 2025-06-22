513ccbb934ba461d58208c41720cb8f6c1b22751
Kyrylo Dulov <kirya4ok1989@gmail.com>
2025-06-22 16:12:44 +0300

    hw18

---
+++ hw18/scripts/script.js
@@ -0,0 +1,11 @@
+const myNum = 10;
+const myStr = 'some string';
+const myBool = true;
+
+decimal2 = myNum;
+console.log(decimal2.toFixed(2));
+
+let myBigInt = 123n;
+let myBigInt1 = 1n
+myBigInt = myBigInt + myBigInt1;
+console.log(myBigInt);
