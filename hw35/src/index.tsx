c477627ce8944165c0a7b866b585a8716de78250
Kyrylo Dulov <kirya4ok1989@gmail.com>
2025-09-08 20:16:25 +0300

    hw36

--- hw35/src/index.tsx
+++ hw35/src/index.tsx
@@ -16,3 +16,59 @@ import './styles.scss';
 
 const container = document.getElementById('root');
 createRoot(container!).render(<App />);
+
+// Task 1
+function sumArray(numbers: any) {
+    return numbers.reduce((acc: any, num: any) => acc + num, 0)
+
+}
+console.log(sumArray([1, 2, 3, 4]))
+console.log(sumArray([]))
+
+// Task2
+type User = {
+    name: string;
+    age: number;
+    isActive: boolean;
+};
+
+function createUser(name: string, age: number, isActive: boolean = true): User {
+    return {
+        name,
+        age,
+        isActive
+    };
+}
+
+const newUser = createUser('Анна', 25, true)
+console.log(newUser)
+
+
+// Task 3
+enum OrderStatus {
+    'Pending',
+    'Shipped',
+    'Delivered',
+    'Cancelled'
+}
+
+function getOrderMessage(status: OrderStatus): string {
+  switch (status) {
+    case OrderStatus.Pending:
+      return "Замовлення очікує на обробку";
+    case OrderStatus.Shipped:
+      return "Замовлення було відправлено";
+    case OrderStatus.Delivered:
+      return "Замовлення доставлено";
+    case OrderStatus.Cancelled:
+      return "Замовлення скасовано";
+    default:
+      throw new Error("Невідомий статус замовлення");
+  }
+}
+
+
+console.log(getOrderMessage(OrderStatus.Pending))
+console.log(getOrderMessage(OrderStatus.Shipped))
+console.log(getOrderMessage(OrderStatus.Delivered))
+console.log(getOrderMessage(OrderStatus.Cancelled))
